ae8a3ed80563a346b51d02adb2091ee53f052aed
Juan Marcos <jncontatojn@gmail.com>
2023-08-31 16:20:49 -0300

    page to build custom plan

--- mahdocki-full-site/src/components/Card/PlanCard/index.js
+++ mahdocki-full-site/src/components/Card/PlanCard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./PlanCard.module.css";
+import image from "assets/images/street paid parking-bro.svg";
 
 const PlanCard = ({ plan, onServiceToggle, onPlanToggle }) => {
   const isSelected = plan.services.every((service) => service.selected);
@@ -7,9 +8,46 @@ const PlanCard = ({ plan, onServiceToggle, onPlanToggle }) => {
   return (
     <>
       <div className={styles.planCard}>
-        <h1>oi</h1>
+        <div className={styles.imageSpace}>
+          <img
+            src={image}
+            alt='none'
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h3 className={styles.titleCard}>{plan.name}</h3>
+          <div className='select-all'>
+            <input
+              type='checkbox'
+              checked={isSelected}
+              onChange={() => onPlanToggle(plan.id, isSelected)}
+            />
+            Selecionar todos os serviços
+          </div>
+          <div className={styles.descriptionActive}>
+            <a href='none'>
+              ▼<span> Mostrar detalhes dos serviços da cobertura </span>
+              <span></span>
+            </a>
+            {plan.services.map((service) => (
+              <div
+                key={service.id}
+                className='service-item'
+              >
+                {service.name}
+                <h3 className={styles.titleCard}>{service.description}</h3>
+                <input
+                  type='checkbox'
+                  checked={service.selected}
+                  onChange={() => onServiceToggle(plan.id, service.id)}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className='plan-card'>
+      {/* <div className='plan-card'>
         <h3>{plan.name}</h3>
         {plan.services.map((service) => (
           <div
@@ -32,7 +70,7 @@ const PlanCard = ({ plan, onServiceToggle, onPlanToggle }) => {
           />
           Selecionar todos os serviços
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
--- mahdocki-full-site/src/pages/Planos/CustomPlan/data.js
+++ mahdocki-full-site/src/pages/Planos/CustomPlan/data.js
@@ -4,33 +4,123 @@ const plans = [
     id: 1,
     name: "Plano A",
     services: [
-      { id: 1, name: "Serviço 1", selected: false },
-      { id: 2, name: "Serviço 2", selected: false },
-      { id: 3, name: "Serviço 3", selected: false },
-      { id: 4, name: "Serviço 4", selected: false },
-      { id: 5, name: "Serviço 5", selected: false },
+      {
+        id: 1,
+        name: "Serviço 1",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 2,
+        name: "Serviço 2",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 3,
+        name: "Serviço 3",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 4,
+        name: "Serviço 4",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 5,
+        name: "Serviço 5",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
     ],
   },
   {
     id: 2,
     name: "Plano B",
     services: [
-      { id: 6, name: "Serviço 6", selected: false },
-      { id: 7, name: "Serviço 7", selected: false },
-      { id: 8, name: "Serviço 8", selected: false },
-      { id: 9, name: "Serviço 9", selected: false },
-      { id: 10, name: "Serviço 10", selected: false },
+      {
+        id: 6,
+        name: "Serviço 6",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 7,
+        name: "Serviço 7",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 8,
+        name: "Serviço 8",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 9,
+        name: "Serviço 9",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 10,
+        name: "Serviço 10",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
     ],
   },
   {
     id: 3,
     name: "Plano C",
     services: [
-      { id: 11, name: "Serviço 11", selected: false },
-      { id: 12, name: "Serviço 12", selected: false },
-      { id: 13, name: "Serviço 13", selected: false },
-      { id: 14, name: "Serviço 14", selected: false },
-      { id: 15, name: "Serviço 15", selected: false },
+      {
+        id: 11,
+        name: "Serviço 11",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 12,
+        name: "Serviço 12",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 13,
+        name: "Serviço 13",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 14,
+        name: "Serviço 14",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
+      {
+        id: 15,
+        name: "Serviço 15",
+        description:
+          "Em casos de emergência você pode bloquear a inguinição do veículo para ele não sair do local atual.",
+        selected: false,
+      },
     ],
   },
 ];
